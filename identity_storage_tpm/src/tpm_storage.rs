88ea2f1a7233ef913ad7551d01d92b4342099816
Michele Festa <michele.festa@linksfoundation.com>
2024-09-17 17:14:46 +0200

    free_handle function

--- identity_storage_tpm/src/tpm_storage.rs
+++ identity_storage_tpm/src/tpm_storage.rs
@@ -2,7 +2,8 @@ use std::sync::{Arc, Mutex};
 
 use identity_jose::jws::JwsAlgorithm;
 use identity_storage::{KeyStorageError, KeyStorageErrorKind, KeyStorageResult, KeyType};
-use tss_esapi::{Context, Tcti};
+use tss_esapi::{constants::StartupType, handles::TpmHandle, structures::CapabilityData, tcti_ldr::NetworkTPMConfig, Context, Tcti};
+use anyhow::{anyhow, Result};
 
 /// Supported key types for TPM Storage
 #[derive(Debug)]
@@ -35,10 +36,15 @@ pub struct TpmStorage{
     ctx: Arc<Mutex<Context>>
 }
 
+
+const HANDLE_STORAGE_FIRST_INDEX:u32 = 0x81008000;
+const HANDLE_STORAGE_LAST_INDEX:u32 = 0x8100FFFF;
 impl TpmStorage {
-    pub fn new(location: Tcti) -> Result<TpmStorage, tss_esapi::Error>{
-        let ctx = Context::new(location)?;
-        return Ok(TpmStorage{ctx: Arc::new(Mutex::new(ctx))})
+    pub fn new() -> Result<TpmStorage>{
+        let location = Tcti::Mssim(NetworkTPMConfig::default()); //TODO: rimuovere
+        let mut ctx = Context::new(location)?;
+        let _ =ctx.startup(StartupType::Clear)?; // TODO: rimuovere
+        Ok(TpmStorage{ctx: Arc::new(Mutex::new(ctx))})
     }
     
     fn match_kty_with_alg(key_type: &TpmKeyType, alg: &JwsAlgorithm) -> KeyStorageResult<()>{
@@ -49,4 +55,41 @@ impl TpmStorage {
             ).with_custom_message(format!("Cannot use keytype {key_type} with algorithm {alg}")))
         }
     }
+
+    /// This function returns an handle address that can be used for persistent storage in the Owner Hierarchy.
+    /// The return value is the first empty handler starting from `HANDLE_STORAGE_FIRST_INDEX`
+    pub fn get_free_handle(&self) -> Result<u32>{
+        let mut ctx = match self.ctx.lock(){
+            Ok(ctx) => ctx,
+            Err(e) => return Err(anyhow!("Cannot retrieve the TPM device"))
+        };
+        
+        // init reading loop of handles
+        let mut more_data = true;
+        let mut handles = vec![];
+        while more_data {
+            let cap_data;
+            (cap_data, more_data) = ctx.get_capability(tss_esapi::constants::CapabilityType::Handles, HANDLE_STORAGE_FIRST_INDEX, HANDLE_STORAGE_LAST_INDEX)?;
+            let handle_list = match cap_data {
+                CapabilityData::Handles(list) => list,
+                _ => return Err(anyhow!("Unexpected result from TPM"))
+            };
+            let filtered = handle_list.iter().filter_map(|handle| match handle{
+                TpmHandle::Persistent(h) => Some(h),
+                _ => None
+            }).map(|persistent| {u32::from(*persistent)});
+
+            handles.extend(filtered);
+        }
+
+        // Finding the first handle not in the list, starting from the first index.
+        let free_handle = match (HANDLE_STORAGE_FIRST_INDEX..=HANDLE_STORAGE_LAST_INDEX)
+        .find(|address| {!handles.contains(address)}){
+            Some(handle) => handle,
+            None => return Err(anyhow!("The persistent storage is full!"))
+        };
+
+        Ok(free_handle)
+    }
+
 }
